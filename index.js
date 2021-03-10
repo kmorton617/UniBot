0211165f3dd95767abe8ad63f5378ddd66e51825
Kevin Morton <kmorton617@gmail.com>
2021-03-10 18:32:24 -0500

    !gif functional

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 console.log('Yo wassup');
 
+require("dotenv").config();
+
+const fetch = require('node-fetch');
+
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login();
+client.login(process.env.BOTTOKEN);
 client.on('ready', readyDiscord);
 
 function readyDiscord() {
@@ -21,14 +25,30 @@ const saySomethingFunny = [
 
 client.on('message', gotMessage);
 
-function gotMessage(msg) {
+async function gotMessage(msg) {
     if (msg.channel.id == '819323001404653592') {
-        if (msg.content === 'yell') {
+
+        let tokens = msg.content.split(' ');
+        let command = tokens[0];
+
+        if (command === '!yell') {
             msg.channel.send('I DONT LIKE CONFRONTATION');
         }
-        else if (msg.content === 'say something funny') {
+        else if (command === '!joke') {
             const index = Math.floor(Math.random() * saySomethingFunny.length);
             msg.channel.send(saySomethingFunny[index]);
+        }
+        else if (command == '!gif') {
+            let keywords = 'funny';
+            if (tokens.length > 1) {
+                keywords = tokens.slice(1, tokens.length).join(' ');
+            }
+            let url = `https://g.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}`;
+            let reponse = await fetch(url);
+            let json = await reponse.json();
+            const index = Math.floor(Math.random() * json.results.length);
+            msg.channel.send('Search Term: ' + keywords);
+            msg.channel.send(json.results[index].url);
         }
     }
 }
